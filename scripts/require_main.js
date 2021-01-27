059c2aa72b344f7bb6004a33e6cc36f3941a3f1d
SpunkyAardvark123 <ktsanter@gmail.com>
2021-01-27 13:27:09 -0500

    relocated to server
    replaced with dummy

--- scripts/require_main.js
+++
@@ -1,8 +0,0 @@
-define(function (require) {
-  require('main');
-  require('standard_notice');
-  require('google_webapp_interface');
-  require('create_element');
-  
-  document.addEventListener('DOMContentLoaded', app.init());
-});
